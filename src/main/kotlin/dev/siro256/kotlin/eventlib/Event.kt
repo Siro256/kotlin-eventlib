9d0cfdbf9fbe0dc05d6b28a54888655c98dd4426
Siro_256 <kai0917@outlook.jp>
2020-11-04 04:39:21 +0900

    Change wording
    なんか気持ち悪かった

--- src/main/kotlin/dev/siro256/kotlin/eventlib/Event.kt
+++ src/main/kotlin/dev/siro256/kotlin/eventlib/Event.kt
@@ -1,14 +1,14 @@
 package dev.siro256.kotlin.eventlib
 
 /**
- * このクラスをextendsしてEventManagerにハンドラを登録することで、
- * call()が呼ばれた際にイベントハンドラが呼び出される
+ * このクラスを継承して[EventManager.registerHandler]でハンドラを登録することで、
+ * [Event.call]が呼ばれた際にイベントハンドラが呼び出される
  */
 
 abstract class Event {
 
     /**
-     * この関数を呼び出すことで、このクラスのハンドラが実行される
+     * この関数を呼び出すことで、[EventManager.handlers]に登録されているハンドラが呼び出される
      */
 
     open fun call() {
--- src/main/kotlin/dev/siro256/kotlin/eventlib/EventHandler.kt
+++ src/main/kotlin/dev/siro256/kotlin/eventlib/EventHandler.kt
@@ -3,7 +3,8 @@ package dev.siro256.kotlin.eventlib
 /**
  * 関数の引数にEventを取り、このアノテーションを付けて
  * [EventManager.registerHandler]を呼び出すことで、
- * 引数のEventが発生した際にメソッドが呼び出される
+ * [Event.call]または[EventManager.callEvent]が呼び出された際に
+ * 関数が実行される
  */
 
 @Target(AnnotationTarget.FUNCTION)
